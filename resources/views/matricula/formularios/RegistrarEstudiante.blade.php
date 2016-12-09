8037d44640645dd703171b76d9bf11378fe76d5c
German D. Hernández <gdhernandezp@gmail.com>
2016-12-08 23:18:59 -0600

    Arreglo enfermedad

--- resources/views/matricula/formularios/RegistrarEstudiante.blade.php
+++ resources/views/matricula/formularios/RegistrarEstudiante.blade.php
@@ -73,11 +73,11 @@
         <label class="radio-inline">{!! Form::radio('enfermedadRadio','1') !!}Si</label>
         <label class="radio-inline">{!! Form::radio('enfermedadRadio','2', true) !!}No</label>
         <span class="input-group-addon" id="nombreEnfermedad">Nombre de la Enfermedad</span>
-        {{Form::text('enfermedadEstudiante[]',null, ['class'=>'form-control', 'placeholder'=>'Nombre de la enfermedad', 'aria-describedby'=>'basic-addon1'])}}
+        {{Form::text('nombreEnfermedad[]',null, ['class'=>'form-control', 'placeholder'=>'Nombre de la enfermedad', 'aria-describedby'=>'basic-addon1'])}}
     </div>
     <div class="input-group form-group">
         <span class="input-group-addon" id="basic-addon1">¿Posee Tratamiento Médico o Medicamentos?</span>
-        {{Form::text('tratamientoEnfermedad[]',null, ['class'=>'form-control', 'placeholder'=>'Tratamiento/Medicamento', 'aria-describedby'=>'basic-addon1'])}}
+        {{Form::text('TratamientoEnfermedad[]',null, ['class'=>'form-control', 'placeholder'=>'Tratamiento/Medicamento', 'aria-describedby'=>'basic-addon1'])}}
     </div>
     <div class="input-group form-group">
         <span class="input-group-addon" id="basic-addon1">EL/LA ESTUDIANTE SE RETIRA DE LA INSTITUCIÓN A LA HORA DE SALIDA: </span>
