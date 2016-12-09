c5dcfeed9a90dcd0e76f080fcf05f14514d7ef36
Todociber <alexlaley10@gmail.com>
2016-12-08 22:30:41 -0600

    Insert Completado

--- app/Http/Controllers/InscriptionController.php
+++ app/Http/Controllers/InscriptionController.php
@@ -12,7 +12,9 @@ use App\Models\Matricula;
 use App\Models\Matriculadocumento;
 use App\Models\Municipio;
 use App\Models\Oficio;
+use App\Models\Padredefamilium;
 use App\Models\Sacramentousuario;
+use App\Models\Telefono;
 use App\Models\User;
 use App\Utilities\Action;
 use DB;
@@ -55,9 +57,7 @@ class InscriptionController extends Controller
 
         $carnetEstudiate = $action->generarCarnet($request['nombreEstudiante'],$request['apellido']);
         $emailEstudiante =$request['correoEstudiante'];
-        $emailPadre ="";
-        $emailMadre ="";
-        $emailResponsable ="";
+
 
 
         if($request['correoEstudiante']==NULL){
@@ -69,7 +69,7 @@ class InscriptionController extends Controller
             'apellido'=>$request['apellido'],
             'genero'=>1,
             'email'=>$emailEstudiante,
-            'password'=>$carnetEstudiate,
+            'password'=>bcrypt($carnetEstudiate),
             'idTipousuario'=>1,
         ]);
         $usuarioEstudiante->save();
@@ -116,7 +116,15 @@ class InscriptionController extends Controller
                 $sacramentosEstudiante->save();
             }
         }
-
+        if($request['TelefonoEmergenciaNombre']!=NULL){
+            $telefonoEmergencia = new Telefono();
+            $telefonoEmergencia->fill([
+                'telefono'=>$request['TelefonoEmergenciaNombre'],
+                'idTipoTelefono'=>4,
+                'idUsuario'=>$usuarioEstudiante->id,
+            ]);
+            $telefonoEmergencia->save();
+        }
 
         if($request['residenciaEstudianteEmergencia']!=NULL){
             $direccionEmergencia = new Direccione();
@@ -177,25 +185,194 @@ class InscriptionController extends Controller
 
         //Inscripcion de Padre
         if($request['nombrePadre']!=NULL&&$request['apellidosPadre']!=NULL && $request['DUIpadre']!=NULL ){
+            $emailPadre= $request['correoPadre'];
+            if($emailPadre==NULL){
+                $emailEstudiante = $carnetEstudiate.'@padrescolegiosjb.net';
+            }
+
             $usuarioPadre = new User();
             $usuarioPadre->fill([
                 'nombre'=>$request['nombrePadre'],
                 'apellido'=>$request['apellidosPadre'],
                 'genero'=>1,
+                'email'=>$emailEstudiante,
+                'password'=>bcrypt($request['DUIpadre']),
+                'idTipousuario'=>2,
             ]);
+            $usuarioPadre->save();
+
+            $padreDeFamilia = new Padredefamilium();
+            $padreDeFamilia->fill([
+                'fechaNacimiento'=>$request['fechaNacimientoPadre'],
+                'DUI'=>$request['DUIpadre'],
+                'nombreLugarTrabajo'=>$request['lugarTrabajoPadre'],
+                'idUsuario'=>$usuarioPadre->id,
+                'idOficio'=>$request['oficiosPadre'],
+                'idTipoPadre'=>1,
+                'idEstadoCivil'=>$request['estadoCivilPadre']
+            ]);
+
+            $padreDeFamilia->save();
+
+            if($request['telefonoTrabajoPadre']!=NULL){
+                $telefonoPadre = new Telefono();
+                $telefonoPadre->fill([
+                    'telefono'=>$request['telefonoTrabajoPadre'],
+                'idTipoTelefono'=>2,
+                'idUsuario'=>$usuarioPadre->id,
+                ]);
+                $telefonoPadre->save();
+            }
+            if($request['DirecciónPadre']!=NULL){
+                $direccioNpadre = new Direccione();
+                $direccioNpadre->fill([
+                    'detalle'=>$request['DirecciónPadre'],
+                    'idMunicipio'=>$request['municipioTrabajoPadre'],
+                    'idTipoDireccion'=>3,
+                    'idUsuario'=>$usuarioPadre->id
+                ]);
+                $direccioNpadre->save();
+            }
+
+            if(count ($request['sacramentosPadre'])>0){
+                $sacramentosPadres = new Sacramentousuario();
+                foreach ($request['sacramentosPadre'] as $sacramento){
+                    $sacramentosPadres->fill([
+                        'idSacramento'=>$sacramento,
+                        'idUsuario'=>$usuarioPadre->id,
+                    ]);
+                    $sacramentosPadres->save();
+                }
+            }
         }
 
 
 
         //Inscripcion de Madres
         if($request['nombreMadre']!=NULL&&$request['apellidoMadre']!=NULL && $request['DUIMadre']!=NULL ){
+            $emailPadre= $request['correoMadre'];
+            if($emailPadre==NULL){
+                $emailEstudiante = $carnetEstudiate.'@madrescolegiosjb.net';
+            }
 
+            $usuarioPadre = new User();
+            $usuarioPadre->fill([
+                'nombre'=>$request['nombreMadre'],
+                'apellido'=>$request['apellidoMadre'],
+                'genero'=>2,
+                'email'=>$emailEstudiante,
+                'password'=>bcrypt($request['DUIMadre']),
+                'idTipousuario'=>2,
+            ]);
+            $usuarioPadre->save();
+
+            $padreDeFamilia = new Padredefamilium();
+            $padreDeFamilia->fill([
+                'fechaNacimiento'=>$request['fechaNacimientoMadre'],
+                'DUI'=>$request['DUIMadre'],
+                'nombreLugarTrabajo'=>$request['lugarTrabajoMadre'],
+                'idUsuario'=>$usuarioPadre->id,
+                'idOficio'=>$request['oficioMadre'],
+                'idTipoPadre'=>1,
+                'idEstadoCivil'=>$request['estadoCivilMadre']
+            ]);
+
+            $padreDeFamilia->save();
+
+            if($request['telefonoMadre']!=NULL){
+                $telefonoPadre = new Telefono();
+                $telefonoPadre->fill([
+                    'telefono'=>$request['telefonoMadre'],
+                    'idTipoTelefono'=>2,
+                    'idUsuario'=>$usuarioPadre->id,
+                ]);
+                $telefonoPadre->save();
+            }
+            if($request['DirecciónMadre']!=NULL){
+                $direccioNpadre = new Direccione();
+                $direccioNpadre->fill([
+                    'detalle'=>$request['DirecciónMadre'],
+                    'idMunicipio'=>$request['municipioTrabajoMadre'],
+                    'idTipoDireccion'=>3,
+                    'idUsuario'=>$usuarioPadre->id
+                ]);
+                $direccioNpadre->save();
+            }
+
+            if(count ($request['sacramentosMadre'])>0){
+                $sacramentosPadres = new Sacramentousuario();
+                foreach ($request['sacramentosMadre'] as $sacramento){
+                    $sacramentosPadres->fill([
+                        'idSacramento'=>$sacramento,
+                        'idUsuario'=>$usuarioPadre->id,
+                    ]);
+                    $sacramentosPadres->save();
+                }
+            }
         }
 
 
         //Inscripcion de Encargado
         if($request['nombreResponsable']!=NULL&&$request['apellidoResponsable']!=NULL && $request['DUIResponsable']!=NULL ){
+            $emailPadre= $request['correoResponsable'];
+            if($emailPadre==NULL){
+                $emailEstudiante = $carnetEstudiate.'@encargadocolegiosjb.net';
+            }
 
+            $usuarioPadre = new User();
+            $usuarioPadre->fill([
+                'nombre'=>$request['nombreResponsable'],
+                'apellido'=>$request['apellidoResponsable'],
+                'genero'=>$request['generoResponsable'],
+                'email'=>$emailEstudiante,
+                'password'=>bcrypt($request['DUIResponsable']),
+                'idTipousuario'=>2,
+            ]);
+            $usuarioPadre->save();
+
+            $padreDeFamilia = new Padredefamilium();
+            $padreDeFamilia->fill([
+                'fechaNacimiento'=>$request['FechaNacimientoResponsable'],
+                'DUI'=>$request['DUIResponsable'],
+                'nombreLugarTrabajo'=>$request['lugarTrabajoResponsable'],
+                'idUsuario'=>$usuarioPadre->id,
+                'idOficio'=>$request['oficioResponsable'],
+                'idTipoPadre'=>1,
+                'idEstadoCivil'=>$request['estadoCivilResponsable']
+            ]);
+
+            $padreDeFamilia->save();
+
+            if($request['telefonoMadre']!=NULL){
+                $telefonoPadre = new Telefono();
+                $telefonoPadre->fill([
+                    'telefono'=>$request['telefonoTrabajoResponsable'],
+                    'idTipoTelefono'=>2,
+                    'idUsuario'=>$usuarioPadre->id,
+                ]);
+                $telefonoPadre->save();
+            }
+            if($request['DireccionTrabajoResponsable']!=NULL){
+                $direccioNpadre = new Direccione();
+                $direccioNpadre->fill([
+                    'detalle'=>$request['DireccionTrabajoResponsable'],
+                    'idMunicipio'=>$request['municipioTrabajoResponsable'],
+                    'idTipoDireccion'=>3,
+                    'idUsuario'=>$usuarioPadre->id
+                ]);
+                $direccioNpadre->save();
+            }
+
+            if(count ($request['sacramentosMadre'])>0){
+                $sacramentosPadres = new Sacramentousuario();
+                foreach ($request['sacramentosResponsable'] as $sacramento){
+                    $sacramentosPadres->fill([
+                        'idSacramento'=>$sacramento,
+                        'idUsuario'=>$usuarioPadre->id,
+                    ]);
+                    $sacramentosPadres->save();
+                }
+            }
         }
 
 
