28f8a051f748e745ffe656c74e1af5e1eb77458c
German D. Hernández <gdhernandezp@gmail.com>
2016-12-14 13:16:30 -0600

    Perfil publico de docente,  y galería de fotos

--- resources/views/main/galeria.blade.php
+++ resources/views/main/galeria.blade.php
@@ -9,12 +9,12 @@
         <div class="row">
             <div class="col-xs-6 col-md-3">
                 <a href="#" class="thumbnail">
-                    <img src="img/galeria/galeria1.jpg"  id="myImg" width="300" height="200">
+                    <img src="img/galeria/galeria1.jpg"  id="myImg" >
                 </a>
             </div>
-            <div class="col-xs-6 col-md-3">
+            <div class="col-xs-6 col-md-2">
                 <a href="#" class="thumbnail">
-                    <img src="img/galeria/galeria2.jpg"  id="myImg2" width="300" height="200">
+                    <img  src="img/galeria/galeria2.jpg"  id="myImg2">
                 </a>
             </div>
             <div class="col-xs-6 col-md-3">
@@ -49,52 +49,52 @@
             </div>
             <div class="col-xs-6 col-md-3">
                 <a href="#" class="thumbnail">
-                    <img src="img/galeria/galeria9.jpg"  id="myImg5" width="300" height="200">
+                    <img src="img/galeria/galeria9.jpg"  id="myImg9" width="300" height="200">
                 </a>
             </div>
             <div class="col-xs-6 col-md-3">
                 <a href="#" class="thumbnail">
-                    <img src="img/galeria/galeria10.jpg"  id="myImg5" width="300" height="200">
+                    <img src="img/galeria/galeria10.jpg"  id="myImg10" width="300" height="200">
                 </a>
             </div>
             <div class="col-xs-6 col-md-3">
                 <a href="#" class="thumbnail">
-                    <img src="img/galeria/galeria11.jpg"  id="myImg5" width="300" height="200">
+                    <img src="img/galeria/galeria11.jpg"  id="myImg11" width="300" height="200">
                 </a>
             </div>
             <div class="col-xs-6 col-md-3">
                 <a href="#" class="thumbnail">
-                    <img src="img/galeria/galeria12.jpg"  id="myImg5" width="300" height="200">
+                    <img src="img/galeria/galeria12.jpg"  id="myImg12" width="300" height="200">
                 </a>
             </div>
             <div class="col-xs-6 col-md-3">
                 <a href="#" class="thumbnail">
-                    <img src="img/galeria/galeria13.jpg"  id="myImg5" width="300" height="200">
+                    <img src="img/galeria/galeria13.jpg"  id="myImg13" width="300" height="200">
                 </a>
             </div>
             <div class="col-xs-6 col-md-3">
                 <a href="#" class="thumbnail">
-                    <img src="img/galeria/galeria14.jpg"  id="myImg5" width="300" height="200">
+                    <img src="img/galeria/galeria14.jpg"  id="myImg14" width="300" height="200">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-2">
+                <a href="#" class="thumbnail">
+                    <img src="img/galeria/galeria15.jpg"  id="myImg15" width="300" height="200">
                 </a>
             </div>
             <div class="col-xs-6 col-md-3">
                 <a href="#" class="thumbnail">
-                    <img src="img/galeria/galeria15.jpg"  id="myImg5" width="300" height="200">
+                    <img src="img/galeria/galeria16.jpg"  id="myImg16" width="300" height="200">
                 </a>
             </div>
             <div class="col-xs-6 col-md-3">
                 <a href="#" class="thumbnail">
-                    <img src="img/galeria/galeria16.jpg"  id="myImg5" width="300" height="200">
+                    <img src="img/galeria/galeria17.jpg"  id="myImg17" width="300" height="200">
                 </a>
             </div>
             <div class="col-xs-6 col-md-3">
                 <a href="#" class="thumbnail">
-                    <img src="img/galeria/galeria17.jpg"  id="myImg5" width="300" height="200">
-                </a>
-            </div>
-            <div class="col-xs-6 col-md-3">
-                <a href="#" class="thumbnail">
-                    <img src="img/galeria/galeria18.jpg"  id="myImg5" width="300" height="200">
+                    <img src="img/galeria/galeria18.jpg"  id="myImg18" width="300" height="200">
                 </a>
             </div>
         </div>
@@ -148,12 +148,194 @@
         <!-- Modal Caption (Image Text) -->
         <div id="caption"></div>
     </div>
+    <!-- The Modal -->
+    <div id="myModal5" class="modal">
+
+        <!-- The Close Button -->
+        <span class="close" onclick="document.getElementById('myModal5').style.display='none'">&times;</span>
+
+        <!-- Modal Content (The Image) -->
+        <img class="modal-content" id="galeria5">
+
+        <!-- Modal Caption (Image Text) -->
+        <div id="caption"></div>
+    </div>
+    <!-- The Modal -->
+    <div id="myModal6" class="modal">
+
+        <!-- The Close Button -->
+        <span class="close" onclick="document.getElementById('myModal6').style.display='none'">&times;</span>
+
+        <!-- Modal Content (The Image) -->
+        <img class="modal-content" id="galeria6">
+
+        <!-- Modal Caption (Image Text) -->
+        <div id="caption"></div>
+    </div>
+    <!-- The Modal -->
+    <div id="myModal7" class="modal">
+
+        <!-- The Close Button -->
+        <span class="close" onclick="document.getElementById('myModal7').style.display='none'">&times;</span>
+
+        <!-- Modal Content (The Image) -->
+        <img class="modal-content" id="galeria7">
+
+        <!-- Modal Caption (Image Text) -->
+        <div id="caption"></div>
+    </div>
+    <!-- The Modal -->
+    <div id="myModal8" class="modal">
+
+        <!-- The Close Button -->
+        <span class="close" onclick="document.getElementById('myModal8').style.display='none'">&times;</span>
+
+        <!-- Modal Content (The Image) -->
+        <img class="modal-content" id="galeria8">
+
+        <!-- Modal Caption (Image Text) -->
+        <div id="caption"></div>
+    </div>
+    <!-- The Modal -->
+    <div id="myModal9" class="modal">
+
+        <!-- The Close Button -->
+        <span class="close" onclick="document.getElementById('myModal9').style.display='none'">&times;</span>
+
+        <!-- Modal Content (The Image) -->
+        <img class="modal-content" id="galeria9">
+
+        <!-- Modal Caption (Image Text) -->
+        <div id="caption"></div>
+    </div>
+    <!-- The Modal -->
+    <div id="myModal10" class="modal">
+
+        <!-- The Close Button -->
+        <span class="close" onclick="document.getElementById('myModal10').style.display='none'">&times;</span>
+
+        <!-- Modal Content (The Image) -->
+        <img class="modal-content" id="galeria10">
+
+        <!-- Modal Caption (Image Text) -->
+        <div id="caption"></div>
+    </div>
+    <!-- The Modal -->
+    <div id="myModal11" class="modal">
+
+        <!-- The Close Button -->
+        <span class="close" onclick="document.getElementById('myModal11').style.display='none'">&times;</span>
+
+        <!-- Modal Content (The Image) -->
+        <img class="modal-content" id="galeria11">
+
+        <!-- Modal Caption (Image Text) -->
+        <div id="caption"></div>
+    </div>
+    <!-- The Modal -->
+    <div id="myModal12" class="modal">
+
+        <!-- The Close Button -->
+        <span class="close" onclick="document.getElementById('myModal12').style.display='none'">&times;</span>
+
+        <!-- Modal Content (The Image) -->
+        <img class="modal-content" id="galeria12">
+
+        <!-- Modal Caption (Image Text) -->
+        <div id="caption"></div>
+    </div>
+    <!-- The Modal -->
+    <div id="myModal13" class="modal">
+
+        <!-- The Close Button -->
+        <span class="close" onclick="document.getElementById('myModal13').style.display='none'">&times;</span>
+
+        <!-- Modal Content (The Image) -->
+        <img class="modal-content" id="galeria13">
+
+        <!-- Modal Caption (Image Text) -->
+        <div id="caption"></div>
+    </div>
+    <!-- The Modal -->
+    <div id="myModal14" class="modal">
+
+        <!-- The Close Button -->
+        <span class="close" onclick="document.getElementById('myModal14').style.display='none'">&times;</span>
+
+        <!-- Modal Content (The Image) -->
+        <img class="modal-content" id="galeria14">
+
+        <!-- Modal Caption (Image Text) -->
+        <div id="caption"></div>
+    </div>
+    <!-- The Modal -->
+    <div id="myModal15" class="modal">
+
+        <!-- The Close Button -->
+        <span class="close" onclick="document.getElementById('myModal15').style.display='none'">&times;</span>
+
+        <!-- Modal Content (The Image) -->
+        <img class="modal-content" id="galeria15">
+
+        <!-- Modal Caption (Image Text) -->
+        <div id="caption"></div>
+    </div>
+    <!-- The Modal -->
+    <div id="myModal16" class="modal">
+
+        <!-- The Close Button -->
+        <span class="close" onclick="document.getElementById('myModal16').style.display='none'">&times;</span>
+
+        <!-- Modal Content (The Image) -->
+        <img class="modal-content" id="galeria16">
+
+        <!-- Modal Caption (Image Text) -->
+        <div id="caption"></div>
+    </div>
+    <!-- The Modal -->
+    <div id="myModal17" class="modal">
+
+        <!-- The Close Button -->
+        <span class="close" onclick="document.getElementById('myModal17').style.display='none'">&times;</span>
+
+        <!-- Modal Content (The Image) -->
+        <img class="modal-content" id="galeria17">
+
+        <!-- Modal Caption (Image Text) -->
+        <div id="caption"></div>
+    </div>
+    <!-- The Modal -->
+    <div id="myModal18" class="modal">
+
+        <!-- The Close Button -->
+        <span class="close" onclick="document.getElementById('myModal18').style.display='none'">&times;</span>
+
+        <!-- Modal Content (The Image) -->
+        <img class="modal-content" id="galeria18">
+
+        <!-- Modal Caption (Image Text) -->
+        <div id="caption"></div>
+    </div>
     <script>
         // Get the modal
         var modal = document.getElementById('myModal');
         var modal2 = document.getElementById('myModal2');
         var modal3 = document.getElementById('myModal3');
         var modal4 = document.getElementById('myModal4');
+        var modal5 = document.getElementById('myModal5');
+        var modal6 = document.getElementById('myModal6');
+        var modal7 = document.getElementById('myModal7');
+        var modal8 = document.getElementById('myModal8');
+        var modal9 = document.getElementById('myModal9');
+        var modal10 = document.getElementById('myModal10');
+        var modal11 = document.getElementById('myModal11');
+        var modal12 = document.getElementById('myModal12');
+        var modal13 = document.getElementById('myModal13');
+        var modal14 = document.getElementById('myModal14');
+        var modal15 = document.getElementById('myModal15');
+        var modal16 = document.getElementById('myModal16');
+        var modal17 = document.getElementById('myModal17');
+        var modal18 = document.getElementById('myModal18');
 
         // Get the image and insert it inside the modal - use its "alt" text as a caption
         var img = document.getElementById('myImg');
@@ -164,6 +346,34 @@
         var modalImg3 = document.getElementById("galeria3");
         var img4 = document.getElementById('myImg4');
         var modalImg4 = document.getElementById("galeria4");
+        var img5 = document.getElementById('myImg5');
+        var modalImg5 = document.getElementById("galeria5");
+        var img6 = document.getElementById('myImg6');
+        var modalImg6 = document.getElementById("galeria6");
+        var img7 = document.getElementById('myImg7');
+        var modalImg7 = document.getElementById("galeria7");
+        var img8 = document.getElementById('myImg8');
+        var modalImg8 = document.getElementById("galeria8");
+        var img9 = document.getElementById('myImg9');
+        var modalImg9 = document.getElementById("galeria9");
+        var img10 = document.getElementById('myImg10');
+        var modalImg10 = document.getElementById("galeria10");
+        var img11 = document.getElementById('myImg11');
+        var modalImg11 = document.getElementById("galeria11");
+        var img12 = document.getElementById('myImg12');
+        var modalImg12 = document.getElementById("galeria12");
+        var img13 = document.getElementById('myImg13');
+        var modalImg13 = document.getElementById("galeria13");
+        var img14 = document.getElementById('myImg14');
+        var modalImg14 = document.getElementById("galeria14");
+        var img15 = document.getElementById('myImg15');
+        var modalImg15 = document.getElementById("galeria15");
+        var img16 = document.getElementById('myImg16');
+        var modalImg16 = document.getElementById("galeria16");
+        var img17 = document.getElementById('myImg17');
+        var modalImg17 = document.getElementById("galeria17");
+        var img18 = document.getElementById('myImg18');
+        var modalImg18 = document.getElementById("galeria18");
         var captionText = document.getElementById("caption");
         img.onclick = function(){
             modal.style.display = "block";
@@ -183,6 +393,76 @@
         img4.onclick = function(){
             modal4.style.display = "block";
             modalImg4.src = this.src;
+            captionText.innerHTML = this.alt;
+        }
+        img5.onclick = function(){
+            modal5.style.display = "block";
+            modalImg5.src = this.src;
+            captionText.innerHTML = this.alt;
+        }
+        img6.onclick = function(){
+            modal6.style.display = "block";
+            modalImg6.src = this.src;
+            captionText.innerHTML = this.alt;
+        }
+        img7.onclick = function(){
+            modal7.style.display = "block";
+            modalImg7.src = this.src;
+            captionText.innerHTML = this.alt;
+        }
+        img8.onclick = function(){
+            modal8.style.display = "block";
+            modalImg8.src = this.src;
+            captionText.innerHTML = this.alt;
+        }
+        img9.onclick = function(){
+            modal9.style.display = "block";
+            modalImg9.src = this.src;
+            captionText.innerHTML = this.alt;
+        }
+        img10.onclick = function(){
+            modal10.style.display = "block";
+            modalImg10.src = this.src;
+            captionText.innerHTML = this.alt;
+        }
+        img11.onclick = function(){
+            modal11.style.display = "block";
+            modalImg11.src = this.src;
+            captionText.innerHTML = this.alt;
+        }
+        img12.onclick = function(){
+            modal12.style.display = "block";
+            modalImg12.src = this.src;
+            captionText.innerHTML = this.alt;
+        }
+        img13.onclick = function(){
+            modal13.style.display = "block";
+            modalImg13.src = this.src;
+            captionText.innerHTML = this.alt;
+        }
+        img14.onclick = function(){
+            modal14.style.display = "block";
+            modalImg14.src = this.src;
+            captionText.innerHTML = this.alt;
+        }
+        img15.onclick = function(){
+            modal15.style.display = "block";
+            modalImg15.src = this.src;
+            captionText.innerHTML = this.alt;
+        }
+        img16.onclick = function(){
+            modal16.style.display = "block";
+            modalImg16.src = this.src;
+            captionText.innerHTML = this.alt;
+        }
+        img17.onclick = function(){
+            modal17.style.display = "block";
+            modalImg17.src = this.src;
+            captionText.innerHTML = this.alt;
+        }
+        img18.onclick = function(){
+            modal18.style.display = "block";
+            modalImg18.src = this.src;
             captionText.innerHTML = this.alt;
         }
 
--- resources/views/teachers/profile.blade.php
+++ resources/views/teachers/profile.blade.php
@@ -10,55 +10,214 @@
 
                 <h2 class="text-center">Personal Docente</h2>
             </div>
-            <hr class="divider">
             <!--<div class="panel-body">
                 <h1>Próximamente...</h1>
             </div>-->
             <div class=" container">
                 <div class="panel-body">
                     <div class="col-lg-4">
-                        <img class="img-circle" src="img/perfil.png"
+                        <img class="img-circle" src="img/perfildocente/perfil15.jpg"
                              alt="Generic placeholder image" width="240" height="240">
                     </div>
-                    <p><h2>Profesor Charles Xavier</h2></p>
-                    <p>Director y miembro de los X-men. Como telépata que es, Xavier puede leer, controlar e influir en la
-                        mente humana. Es un genio científico con una habilidad sobrehumana para absorber información, siendo
-                        además una autoridad líder en genética, mutación y poderes psiónicos.</p>
-                </div>
-
-                <hr class="featurette-divider">
-
-                <div class="panel-body">
-                    <div class="col-lg-4">
-                        <img class="img-circle" src="img/perfil.png"
-                             alt="Generic placeholder image" width="240" height="240">
-                    </div>
-                    <p><h2>Scott Summers (Cíclope)</h2></p>
-                    <p>El primer alumno de Xavier y a menudo el líder de campo.</p>
-                </div>
-
-                <hr class="featurette-divider">
-
-                <div class="panel-body">
-                    <div class="col-lg-4">
-                        <img class="img-circle" src="img/perfil.png"
-                             alt="Generic placeholder image" width="240" height="240">
-                    </div>
-                    <p><h2>Dr. Henry Philip "Hank" McCoy (Bestia)</h2></p>
-                    <p>Encargado de relaciones internacionales humano-mutantes.</p>
-                </div>
-
-                <hr class="featurette-divider">
-
-                <div class="panel-body">
-                    <div class="col-lg-4">
-                        <img class="img-circle" src="img/perfil.png"
-                             alt="Generic placeholder image" width="240" height="240">
-                    </div>
-                    <p><h2>Jean Grey</h2></p>
-                    <p>Poderosa telépata portadora de la fuerza Fenix.</p>
+                    <h2>Antonia Barrera de Martínez</h2>
+                    <h3>Directora</h3>
+                    <h4>Profesora normalista con 45 años de experiencia en el área de la educación.</h4>
                 </div>
             </div>
+
+            <hr class="divider">
+            <div class=" container">
+                <div class="panel-body">
+                    <div class="col-lg-4">
+                        <img class="img-circle" src="img/perfildocente/perfil13.jpg"
+                             alt="Generic placeholder image" width="240" height="240">
+                    </div>
+                    <h2>Hugo Adiel Hernández</h2>
+                    <h3>Sub-Director</h3>
+                    <h4>Profesor en Lenguaje y Literatura para tercer ciclo, educación básica y
+                    educación media, con 11 años de experiencia en el área de la educación y la subdirección.</h4>
+                </div>
+            </div>
+            <hr class="divider">
+
+            <div class=" container">
+                <div class="panel-body">
+                    <div class="col-lg-4">
+                        <img class="img-circle" src="img/perfildocente/perfil1.jpg"
+                             alt="Generic placeholder image" width="240" height="240">
+                    </div>
+                    <h2>Ana Elizabeth García de Rubio</h2>
+                    <h4>Profesora en educación básica para primero y segundo ciclo con 6 años de experiencia en
+                    el área de la educación.</h4>
+                </div>
+            </div>
+            <hr class="divider">
+
+
+            <div class=" container">
+                <div class="panel-body">
+                    <div class="col-lg-4">
+                        <img class="img-circle" src="img/perfildocente/perfil2.jpg"
+                             alt="Generic placeholder image" width="240" height="240">
+                    </div>
+                    <h2>Elsi Esperanza Alvarado de Rodríguez</h2>
+                    <h4>Licenciada en Educación Parvularia con 10 años de experiencia en los niveles de parvularia y
+                    educación básica.</h4>
+
+                </div>
+            </div>
+            <hr class="divider">
+
+
+            <div class=" container">
+                <div class="panel-body">
+                    <div class="col-lg-4">
+                        <img class="img-circle" src="img/perfildocente/perfil3.jpg"
+                             alt="Generic placeholder image" width="240" height="240">
+                    </div>
+                    <h2>Roxana Beatríz Andrade de Hernández</h2>
+                    <h4>Profesora en Educación Parvularia con 15 años de experiencia en el área de la educación.</h4>
+
+                </div>
+            </div>
+            <hr class="divider">
+
+
+            <div class=" container">
+                <div class="panel-body">
+                    <div class="col-lg-4">
+                        <img class="img-circle" src="img/perfildocente/perfil4.jpg"
+                             alt="Generic placeholder image" width="240" height="240">
+                    </div>
+                    <h2>Hilda Yanira Palacios Arguera</h2>
+                    <h4>Profesora en Educación Parvularia con 16 años de experiencia en el área de la educación.</h4>
+
+                </div>
+            </div>
+            <hr class="divider">
+
+
+            <div class=" container">
+                <div class="panel-body">
+                    <div class="col-lg-4">
+                        <img class="img-circle" src="img/perfildocente/perfil5.jpg"
+                             alt="Generic placeholder image" width="240" height="240">
+                    </div>
+                    <h2>Gladis Elizabeth Alfaro de Alfaro</h2>
+                    <h4>Licenciada en Ciencias de la Educación con 10 años de experiencia en los niveles de
+                        educación básica y parvularia.</h4>
+
+                </div>
+            </div>
+            <hr class="divider">
+
+            <div class=" container">
+                <div class="panel-body">
+                    <div class="col-lg-4">
+                        <img class="img-circle" src="img/perfildocente/perfil6.jpg"
+                             alt="Generic placeholder image" width="240" height="240">
+                    </div>
+                    <h2>Zoila Mabel Cardoza de Rodríguez</h2>
+                    <h4>Profesora en Ciencias Sociales para tercer ciclo, educación básica y educación media,
+                        con 12 años de experiencia en el área de la educación.</h4>
+
+                </div>
+            </div>
+            <hr class="divider">
+
+            <div class=" container">
+                <div class="panel-body">
+                    <div class="col-lg-4">
+                        <img class="img-circle" src="img/perfildocente/perfil7.jpg"
+                             alt="Generic placeholder image" width="240" height="240">
+                    </div>
+                    <h2>María Ruth León Pérez</h2>
+                    <h4>Profesora en Ciencias Sociales para tercer ciclo y educación media, con 6 años de experiencia
+                        en el área de la educación.</h4>
+                </div>
+            </div>
+            <hr class="divider">
+
+            <div class=" container">
+                <div class="panel-body">
+                    <div class="col-lg-4">
+                        <img class="img-circle" src="img/perfildocente/perfil8.jpg"
+                             alt="Generic placeholder image" width="240" height="240">
+                    </div>
+                    <h2>Cecilia Maricela Palacios de Sosa</h2>
+                    <h4>Licenciada en Mercadotécnia y Publicidad e instructora de danza, con 13 años de experiencia en
+                        el área de la instructoría.</h4>
+
+                </div>
+            </div>
+            <hr class="divider">
+
+            <div class=" container">
+                <div class="panel-body">
+                    <div class="col-lg-4">
+                        <img class="img-circle" src="img/perfildocente/perfil9.jpg"
+                             alt="Generic placeholder image" width="240" height="240">
+                    </div>
+                    <h2>Yanira del Carmen Ponce de Valladares</h2>
+                    <h4>Profesora en Educación Parvularia con 10 años de experiencia en el nivel de parvularia y
+                        educación básica.</h4>
+                </div>
+            </div>
+            <hr class="divider">
+
+            <div class=" container">
+                <div class="panel-body">
+                    <div class="col-lg-4">
+                        <img class="img-circle" src="img/perfildocente/perfil10.jpg"
+                             alt="Generic placeholder image" width="240" height="240">
+                    </div>
+                    <h2>Yeni Carolina Mendoza Zamora</h2>
+                    <h4>Licenciada en Ciencias de la Educación con especialidad en Ciencias Naturales para tercer ciclo,
+                        educación básica y educación media, con 2 años de experiencia en el nivel de tercer ciclo.</h4>
+
+                </div>
+            </div>
+            <hr class="divider">
+
+            <div class=" container">
+                <div class="panel-body">
+                    <div class="col-lg-4">
+                        <img class="img-circle" src="img/perfildocente/perfil11.jpg"
+                             alt="Generic placeholder image" width="240" height="240">
+                    </div>
+                    <h2>Ana Lidia de la Cruz Paniagua</h2>
+                    <h4>Licenciada en Ciencias de la Educación con especialidad en Educación Física, Deportes y
+                        Recreación, con 6 años de experiencia en el área de la educaión física.</h4>
+                </div>
+            </div>
+            <hr class="divider">
+
+            <div class=" container">
+                <div class="panel-body">
+                    <div class="col-lg-4">
+                        <img class="img-circle" src="img/perfildocente/perfil14.jpg"
+                             alt="Generic placeholder image" width="240" height="240">
+                    </div>
+                    <h2>Santos Antonio Cruz Mejía</h2>
+                    <h4>Licenciado en Educación en Enseñanza del Idioma Inglés, con 2 años de experiencia en el área de
+                        la educación.</h4>
+                </div>
+            </div>
+            <hr class="divider">
+
+            <div class=" container">
+                <div class="panel-body">
+                    <div class="col-lg-4">
+                        <img class="img-circle" src="img/perfildocente/perfil12.jpg"
+                             alt="Generic placeholder image" width="240" height="240">
+                    </div>
+                    <h2>Wilfredo Molina Ayala</h2>
+                    <h4>Licenciado en Sistemas Informáticos, con 3 años de experiencia en el área de la educación.</h4>
+
+                </div>
+            </div>
+            <hr class="divider">
+
         </div>
     </div>
 
