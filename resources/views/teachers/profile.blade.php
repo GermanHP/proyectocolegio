52df562e972f5e11d9d5c577447c2b08b6c92543
German D. Hernández <gdhernandezp@gmail.com>
2016-12-15 12:05:14 -0600

    Actualización de perfil docente

--- resources/views/teachers/profile.blade.php
+++ resources/views/teachers/profile.blade.php
@@ -32,10 +32,54 @@
                         <img class="img-circle" src="img/perfildocente/perfil13.jpg"
                              alt="Generic placeholder image" width="240" height="240">
                     </div>
-                    <h2>Hugo Adiel Hernández</h2>
-                    <h3>Sub-Director</h3>
-                    <h4>Profesor en Lenguaje y Literatura para tercer ciclo, educación básica y
-                    educación media, con 11 años de experiencia en el área de la educación y la subdirección.</h4>
+                    <h3>Hugo Adiel Hernández</h3>
+                    <h4>Sub-Director</h4>
+                    <h5>Responsable de Cuarto Grado sección B y Profesor de Lenguaje y Literatura para Tercer Ciclo.</h5>
+                    <p>Profesor en Lenguaje y Literatura para tercer ciclo, educación básica y
+                    educación media, con 11 años de experiencia en el área de la educación y la subdirección.</p>
+                </div>
+            </div>
+            <hr class="divider">
+
+            <div class=" container">
+                <div class="panel-body">
+                    <div class="col-lg-4">
+                        <img class="img-circle" src="img/perfildocente/perfil4.jpg"
+                             alt="Generic placeholder image" width="240" height="240">
+                    </div>
+                    <h3>Hilda Yanira Palacios Arguera</h3>
+                    <h4>Responsable de Parvularia 4 Años</h4>
+                    <h5>Profesora en Educación Parvularia con 16 años de experiencia en el área de la educación.</h5>
+
+                </div>
+            </div>
+            <hr class="divider">
+
+            <div class=" container">
+                <div class="panel-body">
+                    <div class="col-lg-4">
+                        <img class="img-circle" src="img/perfildocente/perfil3.jpg"
+                             alt="Generic placeholder image" width="240" height="240">
+                    </div>
+                    <h3>Roxana Beatríz Andrade de Hernández</h3>
+                    <h4>Responsable de Parvularia 5 Años</h4>
+                    <h5>Profesora en Educación Parvularia con 15 años de experiencia en el área de la educación.</h5>
+
+                </div>
+            </div>
+            <hr class="divider">
+
+            <div class=" container">
+                <div class="panel-body">
+                    <div class="col-lg-4">
+                        <img class="img-circle" src="img/perfildocente/perfil2.jpg"
+                             alt="Generic placeholder image" width="240" height="240">
+                    </div>
+                    <h3>Elsi Esperanza Alvarado de Rodríguez</h3>
+                    <h4>Responsable de Parvularia 6 Años</h4>
+                    <h5>Licenciada en Educación Parvularia con 10 años de experiencia en los niveles de parvularia y
+                        educación básica.</h5>
+
                 </div>
             </div>
             <hr class="divider">
@@ -46,66 +90,67 @@
                         <img class="img-circle" src="img/perfildocente/perfil1.jpg"
                              alt="Generic placeholder image" width="240" height="240">
                     </div>
-                    <h2>Ana Elizabeth García de Rubio</h2>
-                    <h4>Profesora en educación básica para primero y segundo ciclo con 6 años de experiencia en
-                    el área de la educación.</h4>
+                    <h3>Ana Elizabeth García de Rubio</h3>
+                    <h4>Responsable de Primer Grado</h4>
+                    <h5>Profesora en educación básica para primero y segundo ciclo con 6 años de experiencia en
+                    el área de la educación.</h5>
                 </div>
             </div>
             <hr class="divider">
 
-
             <div class=" container">
                 <div class="panel-body">
                     <div class="col-lg-4">
-                        <img class="img-circle" src="img/perfildocente/perfil2.jpg"
+                        <img class="img-circle" src="img/perfil.png"
                              alt="Generic placeholder image" width="240" height="240">
                     </div>
-                    <h2>Elsi Esperanza Alvarado de Rodríguez</h2>
-                    <h4>Licenciada en Educación Parvularia con 10 años de experiencia en los niveles de parvularia y
-                    educación básica.</h4>
-
-                </div>
-            </div>
-            <hr class="divider">
-
-
-            <div class=" container">
-                <div class="panel-body">
-                    <div class="col-lg-4">
-                        <img class="img-circle" src="img/perfildocente/perfil3.jpg"
-                             alt="Generic placeholder image" width="240" height="240">
-                    </div>
-                    <h2>Roxana Beatríz Andrade de Hernández</h2>
-                    <h4>Profesora en Educación Parvularia con 15 años de experiencia en el área de la educación.</h4>
+                    <h3>Evelyn Xiomara Barahona</h3>
+                    <h4>Responsable de Segundo Grado</h4>
+                    <h5></h5>
 
                 </div>
             </div>
             <hr class="divider">
 
-
-            <div class=" container">
-                <div class="panel-body">
-                    <div class="col-lg-4">
-                        <img class="img-circle" src="img/perfildocente/perfil4.jpg"
+                <div class=" container">
+                    <div class="panel-body">
+                        <div class="col-lg-4">
+                            <img class="img-circle" src="img/perfildocente/perfil5.jpg"
                              alt="Generic placeholder image" width="240" height="240">
                     </div>
-                    <h2>Hilda Yanira Palacios Arguera</h2>
-                    <h4>Profesora en Educación Parvularia con 16 años de experiencia en el área de la educación.</h4>
+                    <h3>Gladis Elizabeth Alfaro de Alfaro</h3>
+                    <h4>Responsable de Segundo Grado</h4>
+                    <h5>Licenciada en Ciencias de la Educación con 10 años de experiencia en los niveles de
+                        educación básica y parvularia.</h5>
 
                 </div>
             </div>
             <hr class="divider">
 
+            <div class=" container">
+                <div class="panel-body">
+                    <div class="col-lg-4">
+                        <img class="img-circle" src="img/perfildocente/perfil7.jpg"
+                             alt="Generic placeholder image" width="240" height="240">
+                    </div>
+                    <h3>María Ruth León Pérez</h3>
+                    <h4>Responsable de Tercer Grado</h4>
+                    <h5>Profesora en Ciencias Sociales para tercer ciclo y educación media, con 6 años de experiencia
+                        en el área de la educación.</h5>
+                </div>
+            </div>
+            <hr class="divider">
 
             <div class=" container">
                 <div class="panel-body">
                     <div class="col-lg-4">
-                        <img class="img-circle" src="img/perfildocente/perfil5.jpg"
+                        <img class="img-circle" src="img/perfildocente/perfil10.jpg"
                              alt="Generic placeholder image" width="240" height="240">
                     </div>
-                    <h2>Gladis Elizabeth Alfaro de Alfaro</h2>
-                    <h4>Licenciada en Ciencias de la Educación con 10 años de experiencia en los niveles de
-                        educación básica y parvularia.</h4>
+                    <h3>Yeni Carolina Mendoza Zamora</h3>
+                    <h4>Responsable de Cuarto Grado Sección A</h4>
+                    <h5>Licenciada en Ciencias de la Educación con especialidad en Ciencias Naturales para tercer ciclo,
+                        educación básica y educación media, con 2 años de experiencia en el nivel de tercer ciclo.</h5>
 
                 </div>
             </div>
@@ -118,8 +163,10 @@
                              alt="Generic placeholder image" width="240" height="240">
                     </div>
                     <h2>Zoila Mabel Cardoza de Rodríguez</h2>
-                    <h4>Profesora en Ciencias Sociales para tercer ciclo, educación básica y educación media,
-                        con 12 años de experiencia en el área de la educación.</h4>
+                    <h4>Responsable de Quinto Grado y Octavo Grado, y profesora de Estudios Sociales desde Cuarto Grado hasta
+                        Noveno Grado.</h4>
+                    <h5>Profesora en Ciencias Sociales para tercer ciclo, educación básica y educación media,
+                        con 12 años de experiencia en el área de la educación.</h5>
 
                 </div>
             </div>
@@ -128,25 +175,13 @@
             <div class=" container">
                 <div class="panel-body">
                     <div class="col-lg-4">
-                        <img class="img-circle" src="img/perfildocente/perfil7.jpg"
+                        <img class="img-circle" src="img/perfil.png"
                              alt="Generic placeholder image" width="240" height="240">
                     </div>
-                    <h2>María Ruth León Pérez</h2>
-                    <h4>Profesora en Ciencias Sociales para tercer ciclo y educación media, con 6 años de experiencia
-                        en el área de la educación.</h4>
-                </div>
-            </div>
-            <hr class="divider">
-
-            <div class=" container">
-                <div class="panel-body">
-                    <div class="col-lg-4">
-                        <img class="img-circle" src="img/perfildocente/perfil8.jpg"
-                             alt="Generic placeholder image" width="240" height="240">
-                    </div>
-                    <h2>Cecilia Maricela Palacios de Sosa</h2>
-                    <h4>Licenciada en Mercadotécnia y Publicidad e instructora de danza, con 13 años de experiencia en
-                        el área de la instructoría.</h4>
+                    <h3>Flor de María Pérez</h3>
+                    <h4>Responsable de Sexto Grado y Noveno grado, y profesora de Matemáticas desde Cuarto Grado hasta
+                        Noveno Grado</h4>
+                    <h5></h5>
 
                 </div>
             </div>
@@ -158,9 +193,10 @@
                         <img class="img-circle" src="img/perfildocente/perfil9.jpg"
                              alt="Generic placeholder image" width="240" height="240">
                     </div>
-                    <h2>Yanira del Carmen Ponce de Valladares</h2>
-                    <h4>Profesora en Educación Parvularia con 10 años de experiencia en el nivel de parvularia y
-                        educación básica.</h4>
+                    <h3>Yanira del Carmen Ponce de Valladares</h3>
+                    <h4>Profesora Auxiliar en Segundo Ciclo</h4>
+                    <h5>Profesora en Educación Parvularia con 10 años de experiencia en el nivel de parvularia y
+                        educación básica.</h5>
                 </div>
             </div>
             <hr class="divider">
@@ -168,12 +204,13 @@
             <div class=" container">
                 <div class="panel-body">
                     <div class="col-lg-4">
-                        <img class="img-circle" src="img/perfildocente/perfil10.jpg"
+                        <img class="img-circle" src="img/perfildocente/perfil8.jpg"
                              alt="Generic placeholder image" width="240" height="240">
                     </div>
-                    <h2>Yeni Carolina Mendoza Zamora</h2>
-                    <h4>Licenciada en Ciencias de la Educación con especialidad en Ciencias Naturales para tercer ciclo,
-                        educación básica y educación media, con 2 años de experiencia en el nivel de tercer ciclo.</h4>
+                    <h3>Cecilia Maricela Palacios de Sosa</h3>
+                    <h4>Instructora de Danza desde Parvularia hasta Noveno Grado</h4>
+                    <h5>Licenciada en Mercadotécnia y Publicidad e instructora de danza, con 13 años de experiencia en
+                        el área de la instructoría.</h5>
 
                 </div>
             </div>
@@ -185,9 +222,10 @@
                         <img class="img-circle" src="img/perfildocente/perfil11.jpg"
                              alt="Generic placeholder image" width="240" height="240">
                     </div>
-                    <h2>Ana Lidia de la Cruz Paniagua</h2>
-                    <h4>Licenciada en Ciencias de la Educación con especialidad en Educación Física, Deportes y
-                        Recreación, con 6 años de experiencia en el área de la educaión física.</h4>
+                    <h3>Ana Lidia de la Cruz Paniagua</h3>
+                    <h4>Profesora de Educación Física desde Parvularia hasta Noveno Grado.</h4>
+                    <h5>Licenciada en Ciencias de la Educación con especialidad en Educación Física, Deportes y
+                        Recreación, con 6 años de experiencia en el área de la educaión física.</h5>
                 </div>
             </div>
             <hr class="divider">
@@ -198,9 +236,10 @@
                         <img class="img-circle" src="img/perfildocente/perfil14.jpg"
                              alt="Generic placeholder image" width="240" height="240">
                     </div>
-                    <h2>Santos Antonio Cruz Mejía</h2>
-                    <h4>Licenciado en Educación en Enseñanza del Idioma Inglés, con 2 años de experiencia en el área de
-                        la educación.</h4>
+                    <h3>Santos Antonio Cruz Mejía</h3>
+                    <h4>Profesor de Inglés desde Parvularia hasta Noveno Grado</h4>
+                    <h5>Licenciado en Educación en Enseñanza del Idioma Inglés, con 2 años de experiencia en el área de
+                        la educación.</h5>
                 </div>
             </div>
             <hr class="divider">
@@ -211,8 +250,9 @@
                         <img class="img-circle" src="img/perfildocente/perfil12.jpg"
                              alt="Generic placeholder image" width="240" height="240">
                     </div>
-                    <h2>Wilfredo Molina Ayala</h2>
-                    <h4>Licenciado en Sistemas Informáticos, con 3 años de experiencia en el área de la educación.</h4>
+                    <h3>Wilfredo Molina Ayala</h3>
+                    <h4>Profesor de Informática desde Parvularia hasta Noveno Grado</h4>
+                    <h5>Licenciado en Sistemas Informáticos, con 3 años de experiencia en el área de la educación.</h5>
 
                 </div>
             </div>
