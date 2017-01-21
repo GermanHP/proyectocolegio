ac3eadf1cabb5011060b4b39635e729f08287a65
German D. Hernández <gdhernandezp@gmail.com>
2017-01-21 12:34:45 -0600

    Horarios de clases

---
+++ resources/views/horarios/kinderPrepa/kinder5.blade.php
@@ -0,0 +1,97 @@
+<div class="container panel">
+    <h2>Horario de Clases Kinder 5 años.</h2>
+    <table class="table table-hover table-bordered">
+        <thead>
+        <tr>
+            <th>Hora</th>
+            <th>Lunes</th>
+            <th>Martes</th>
+            <th>Mi&eacute;rcoles</th>
+            <th>Jueves</th>
+            <th>Viernes</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>7:30am - 7:45am</td>
+            <td>Bienvenida</td>
+            <td>Bienvenida</td>
+            <td>Bienvenida</td>
+            <td>Bienvenida</td>
+            <td>Bienvenida</td>
+        </tr>
+        <tr>
+            <td>7:45am - 8:20am</td>
+            <td>Danza</td>
+            <td>Ingl&eacute;s</td>
+            <td>Inform&aacute;tica</td>
+            <td>Asamblea</td>
+            <td>Asamblea</td>
+        </tr>
+        <tr>
+            <td>8:20am - 9:00am</td>
+            <td>Juego Trabajo</td>
+            <td>Juego Trabajo</td>
+            <td>Juego Trabajo</td>
+            <td>Educaci&oacute;n en la Fe</td>
+            <td>Juego Trabajo</td>
+        </tr>
+        <tr class="success">
+            <td>9:00am - 9:30am</td>
+            <td>Alimentaci&oacute;n</td>
+            <td>Alimentaci&oacute;n</td>
+            <td>Alimentaci&oacute;n</td>
+            <td>Alimentaci&oacute;n</td>
+            <td>Alimentaci&oacute;n</td>
+        </tr>
+        <tr>
+            <td>9:30am - 10:00am</td>
+            <td>Recreo</td>
+            <td>Educaci&oacute;n F&iacute;sica</td>
+            <td>Recreo</td>
+            <td>Recreo</td>
+            <td>Recreo</td>
+        </tr>
+        <tr class="success">
+            <td>10:00am - 10:10am</td>
+            <td>Descanso</td>
+            <td>Descanso</td>
+            <td>Descanso</td>
+            <td>Descanso</td>
+            <td>Descanso</td>
+        </tr>
+        <tr>
+            <td>10:10am - 10:20am</td>
+            <td>Cuento</td>
+            <td>Cuento</td>
+            <td>Cuento</td>
+            <td>Cuento</td>
+            <td>Cuento</td>
+        </tr>
+        <tr>
+            <td>10:20am - 10:45am</td>
+            <td>Taller</td>
+            <td>Taller</td>
+            <td>Taller</td>
+            <td>Taller</td>
+            <td>Taller</td>
+        </tr>
+        <tr>
+            <td>10:45am - 10:55am</td>
+            <td>Asamblea</td>
+            <td>Asamblea</td>
+            <td>Asamblea</td>
+            <td>Asamblea</td>
+            <td>Asamblea</td>
+        </tr>
+        <tr class="success">
+            <td>10:55am - 11:00am</td>
+            <td>Orden, Aseo y Despedida</td>
+            <td>Orden, Aseo y Despedida</td>
+            <td>Orden, Aseo y Despedida</td>
+            <td>Orden, Aseo y Despedida</td>
+            <td>Orden, Aseo y Despedida</td>
+        </tr>
+        </tbody>
+    </table>
+</div>
