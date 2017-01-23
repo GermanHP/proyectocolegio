a2f28f50bfab6e7c16d495b79d79ce90c0256eda
Todociber <alexlaley10@gmail.com>
2017-01-23 17:26:51 -0600

    cambio de usuarios y contraseñas para moodle

--- database/seeds/SeederHorariosDisponibles.php
+++ database/seeds/SeederHorariosDisponibles.php
@@ -11,6 +11,7 @@ class SeederHorariosDisponibles extends Seeder
      */
     public function run()
     {
+      //Tercer Ciclo
         DB::table('horasdisponibles')->insert(array(
             'horaInicio' => '7:00',
             'horaFin' => '7:45',
@@ -82,5 +83,81 @@ class SeederHorariosDisponibles extends Seeder
             'created_at' => new DateTime,
             'updated_at' => new DateTime
         ));
+
+        //Segundo Ciclo
+        DB::table('horasdisponibles')->insert(array(
+            'horaInicio' => '12:45',
+            'horaFin' => '1:30',
+            'created_at' => new DateTime,
+            'updated_at' => new DateTime
+        ));
+        DB::table('horasdisponibles')->insert(array(
+        'horaInicio' => '1:30',
+        'horaFin' => '2:15',
+        'created_at' => new DateTime,
+        'updated_at' => new DateTime
+    ));DB::table('horasdisponibles')->insert(array(
+        'horaInicio' => '2:30',
+        'horaFin' => '3:15',
+        'created_at' => new DateTime,
+        'updated_at' => new DateTime
+    ));DB::table('horasdisponibles')->insert(array(
+        'horaInicio' => '3:15',
+        'horaFin' => '4:00',
+        'created_at' => new DateTime,
+        'updated_at' => new DateTime
+    ));DB::table('horasdisponibles')->insert(array(
+        'horaInicio' => '4:15',
+        'horaFin' => '5:00',
+        'created_at' => new DateTime,
+        'updated_at' => new DateTime
+    ));DB::table('horasdisponibles')->insert(array(
+        'horaInicio' => '5:00',
+        'horaFin' => '5:30',
+        'created_at' => new DateTime,
+        'updated_at' => new DateTime
+    ));
+
+
+
+
+    //Horarios Primer Ciclo
+    DB::table('horasdisponibles')->insert(array(
+        'horaInicio' => '7:00',
+        'horaFin' => '7:50',
+        'created_at' => new DateTime,
+        'updated_at' => new DateTime
+    ));DB::table('horasdisponibles')->insert(array(
+        'horaInicio' => '7:50',
+        'horaFin' => '8:40',
+        'created_at' => new DateTime,
+        'updated_at' => new DateTime
+    ));DB::table('horasdisponibles')->insert(array(
+        'horaInicio' => '9:00',
+        'horaFin' => '9:45',
+        'created_at' => new DateTime,
+        'updated_at' => new DateTime
+    ));DB::table('horasdisponibles')->insert(array(
+        'horaInicio' => '9:45',
+        'horaFin' => '10:30',
+        'created_at' => new DateTime,
+        'updated_at' => new DateTime
+    ));DB::table('horasdisponibles')->insert(array(
+        'horaInicio' => '10:45',
+        'horaFin' => '11:30',
+        'created_at' => new DateTime,
+        'updated_at' => new DateTime
+    ));DB::table('horasdisponibles')->insert(array(
+        'horaInicio' => '11:30',
+        'horaFin' => '12:00',
+        'created_at' => new DateTime,
+        'updated_at' => new DateTime
+    ));
+
+
+
+
+
+
     }
 }
