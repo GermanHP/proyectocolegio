309f9fc8eb87cfc71185e9de234aff9b68b61819
German D. Hernández <gdhernandezp@gmail.com>
2017-11-03 17:55:23 -0600

    BOLETA DE INFORME FINAL DE RENDIMIENTO ACADÉMICO 2017

---
+++ resources/views/main/boletafinal.blade.php
@@ -0,0 +1,249 @@
+<html>
+<head>
+
+</head>
+<!-- Tell the browser to be responsive to screen width -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<!-- Bootstrap 3.3.5 -->
+<style>
+
+    <?php include(public_path() . '/assets/css/bootstrap.css');?>
+    <?php include(public_path() . '/assets/dist/css/AdminLTE.css');?>
+    <?php include(public_path() . '/assets/dist/css/skins/_all-skins.css');?>
+    <?php include(public_path() . '/assets/css/font-awesome.css');?>
+
+@page teacher {
+        size: A4 ;
+        margin: 1cm;
+    }
+
+    .teacherPage {
+        page: teacher;
+        page-break-after: always;
+    }
+
+    .text-rotation {display: block;
+        -webkit-transform: rotate(-90deg);
+        -moz-transform: rotate(-90deg);
+    }
+
+    .docente{
+        margin-left: 4%;
+    }
+
+    .director{
+        margin-top: -4.5%;
+    }
+
+    .director2{
+        margin-top: -4%;
+        margin-right: 11%;
+    }
+
+    .Rotate-90
+    {
+        -webkit-transform: rotate(-90deg);
+        -moz-transform: rotate(-90deg);
+        -ms-transform: rotate(-90deg);
+        -o-transform: rotate(-90deg);
+        transform: rotate(-90deg);
+
+        position: relative;
+        left:75px;
+
+
+    }
+
+    @table-border-color{
+        table-border-color-dark: #000;
+    }
+    .ajustar{
+        height: 200px;
+        width: 100px;
+        float: left;
+        white-space: pre; /* CSS 2.0 */
+        white-space: pre-wrap; /* CSS 2.1 */
+        white-space: pre-line; /* CSS 3.0 */
+        white-space: -pre-wrap; /* Opera 4-6 */
+        white-space: -o-pre-wrap; /* Opera 7 */
+        white-space: -moz-pre-wrap; /* Mozilla */
+        white-space: -hp-pre-wrap; /* HP */
+        word-wrap: break-word; /* IE 5+ */
+    }
+</style>
+
+<body style="height: 300px;!important; background: white;">
+
+    <div class="row teacherPage">
+        <div class="col-md-12">
+
+            <h2 class="text-center"><img src="https://colegiosjb.net/img/indexes/logo.jpg" alt="" height="100" width="100"><br></h2>
+            <h2 class="page-header text-center" id="titulo">Colegio San Juan Bautista<br><br></h2>
+
+            <h3 class="text-center" id="titulo">INFORME FINAL DE RENDIMIENTO ACADÉMICO 2017<br></h3>
+            <h3 class="text-center" id="titulo">COLEGIO "SAN JUAN BAUTISTA"<br></h3>
+            <h3 class="text-center" id="titulo">GRADO<br><br></h3>
+
+
+            <div class="container">
+                <table class="table" border="1" bordercolor="#0000">
+                    <thead>
+                    <tr>
+                        <td colspan="10"><h4 class="text-center">ALUMNO/A:</h4></td>
+                    </tr>
+
+                    <tr>
+                        <th>PERIODOS</th>
+                        <th rowspan="0"><label class="Rotate-90 ajustar">PRIMER PERIODO</label></th>
+                        <th rowspan="0"><label class="Rotate-90 ajustar">Puntaje</label></th>
+                        <th rowspan="0"><label class="Rotate-90 ajustar">SEGUNDO PERIODO</label></th>
+                        <th rowspan="0"><label class="Rotate-90 ajustar">Puntaje</label></th>
+                        <th rowspan="0"><label class="Rotate-90 ajustar">TERCER PERIODO</label></th>
+                        <th rowspan="0"><label class="Rotate-90 ajustar">Puntaje</label></th>
+                        <th rowspan="0"><label class="Rotate-90 ajustar">CUARTO PERIODO</label></th>
+                        <th rowspan="0"><label class="Rotate-90 ajustar">Puntaje</label></th>
+                        <th rowspan="0"><label class="Rotate-90 ajustar">PROMEDIO FINAL</label></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php $promedioGlobal =0?>
+                    @foreach($alumno->matriculas[0]->gradoseccion->materiagrados as $materiagrado)
+                        <tr>
+                            <td>{{$materiagrado->materium->nombre}}</td>
+
+                            <?php
+                            $rev = false;
+                            $com= false;
+                            $int= false;
+                            $exa= false;
+                            $revision=NULL;
+                            $complementarias =NULL;
+                            $integradoras=NULL;
+                            $examen =NULL;?>
+                            @foreach($materiagrado->notas as $nota)
+
+                                @if($nota->idMateriaGrado == $materiagrado->id)
+                                    @if($nota->idEstudiante == $alumno->id)
+                                        @if($nota->idTipoNota==1)
+
+                                            <td>{{$nota->nota}}</td>
+                                            <td><?php
+                                                $rev=true;
+                                                $revision= round($nota->nota*0.15,2);
+                                                echo $revision?></td>
+
+                                        @endif
+                                        @if($nota->idTipoNota==2)
+
+                                            <td>{{$nota->nota}}</td>
+                                            <td><?php
+                                                $com= true;
+                                                $complementarias=round($nota->nota*0.20,2);
+                                                echo $complementarias ;?></td>
+
+                                        @endif
+                                        @if($nota->idTipoNota==3)
+
+                                            <td>{{$nota->nota}}</td>
+                                            <td><?php
+                                                $int= true;
+                                                $integradoras =round($nota->nota*0.35,2);
+                                                echo $integradoras;?></td>
+
+                                        @endif
+                                        @if($nota->idTipoNota==4)
+
+                                            <td>{{$nota->nota}}</td>
+                                            <td><?php
+                                                $exa=true;
+                                                $examen =round($nota->nota*0.30,2);
+                                                echo $examen?></td>
+
+                                        @endif
+
+                                    @endif
+                                @endif
+
+
+                            @endforeach
+                            @if($rev==false && $com==false && $int==false && $exa==false)
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td><b>0</b></td>
+                            @else
+                                <td><b><?php $promedio = $revision+$complementarias+$integradoras+$examen;
+                                        $promedioGlobal=$promedioGlobal+$promedio;
+                                        echo $promedio?></b>
+                                    @endif
+
+                                </td>
+                        </tr>
+                    @endforeach
+                    <tr>
+                        <td>CONDUCTA</td>
+                        <td colspan="8"></td>
+                        <td>
+                            <?php $nvueltas =0?>
+                            @foreach($alumno->datosboleta as $boleta)
+                                @if($boleta->idPeriodo=1)
+                                    <?php $nvueltas++;
+                                    $promedioGlobal=$promedioGlobal+$boleta->notaConducta;
+                                    ?>
+                                    {{$boleta->notaConducta}}
+                                @endif
+                            @endforeach
+                            @if($nvueltas==0)
+                                0
+                            @endif
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="9">PROMEDIO GLOBAL</td>
+                        <td>{{$promedioGlobal}}</td>
+                    </tr>
+                    <br>
+                    <tr>
+                        <td colspan="3">PORCENTAJE DE ASISTENCIAS</td>
+
+                        <td>
+                            <?php $nvueltas =0?>
+                            @foreach($alumno->datosboleta as $boleta)
+                                @if($boleta->idPeriodo=1)
+                                    <?php $nvueltas++;
+                                    $nasistncia = $boleta->porcentajeAsistencia*10;
+                                    ?>
+                                    {{$nasistncia}}
+
+                                @endif
+                            @endforeach
+                            @if($nvueltas==0)
+                                0
+                            @endif
+                            %</td>
+                    </tr>
+                    </tbody>
+                </table>
+
+                <h4>OBSERVACIONES</h4>
+                <textarea name="observaciones" id="observacion" cols="164" rows="8">@foreach($alumno->datosboleta as $boleta)@if($boleta->idPeriodo=1){{$boleta->Observaciones}}@endif @endforeach</textarea><br><br><br><br><br>
+                <h4>__________________________________</h4>
+                <h6 class="docente">DOCENTE RESPONSABLE DE SECCIÓN</h6>
+                <h4 class="pull-right director">__________________________________</h4><br>
+                <h6 class="pull-right director2">DIRECTOR/A</h6>
+            </div>
+        </div><!-- /.col -->
+    </div><!-- /.col -->
+    </div><!-- /.row -->
+
+    <?php $nVueltas++?>
+
+
+@endforeach
+</body>
+</html>
